30ca1709184a4817b676e99c30c7155db8b1c838
Moyok <songxiaoxu2002@gmail.com>
2022-10-14 11:24:04 +0800

    初始化项目

---
+++ src/main/java/com/xiaoxu/learn/MybatisPlusLearnApplication.java
@@ -0,0 +1,13 @@
+package com.xiaoxu.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisPlusLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisPlusLearnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xiaoxu/learn/MybatisPlusLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaoxu.learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisPlusLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
